e4bddc7d36c01bbc42834ae84ce7176d8f16e827
David Joseph <56838866+davidcbalsley@users.noreply.github.com>
2020-02-04 21:31:21 -0600

    experimenting with stackexchange

---
+++ script.js
@@ -0,0 +1,8 @@
+SE.init({
+    clientId: 17151,
+    key: '2DTX0ttCl8HX5AirFtN7)Q((',
+    channelUrl: 'https://davidcbalsley.github.io/studybuddy/blank',
+    complete: function (data) {
+        // Don't do anything yet
+    }
+});
